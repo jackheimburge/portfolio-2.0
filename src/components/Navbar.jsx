d149d6b48e9a048a0fca5730236f6cb738519515
centipedejam <jheimburge@gmail.com>
2023-12-30 00:08:35 -0800

    set up hero bg-img and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,6 @@ const Navbar = () => {
             alt="logo"
             className="w-9 h-9 object-contain"
           />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">Jack Heimburge</p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
